d4c7bd28767c2635895db95f5dd3debbd55b7547
Konlawat Chuabswasdi <6410301016@cdti.ac.th>
2022-12-21 10:37:43 +0700

    image in html file

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 const worker = require("./model")
 var path = require('path');
 var filename = path.basename
-
+var public = require('path').join(__dirname,'/html')
+app.use(express.static(public))
 app.get('/', (req, res)=>{
     //res.send('Hello world from Express Framework')
     res.sendFile(__dirname + "/html/main.html")
@@ -22,7 +23,7 @@ app.get('/File',function(req, res){
     res.sendFile( __dirname + "/html/about.html")
 })
 app.get('*', (req, res)=>{
-    res.sendFile(__dirname + "/static/404.jpg")
+    res.sendFile(__dirname + "/html/404.jpg")
 })
 app.listen(3000, ()=>{
     console.log("Start Server at Port [3000]")
